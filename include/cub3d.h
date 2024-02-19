66ba7d4c50bcfd2c716aa9d395468f903bec6ae2
Jiajia Chen <jiajchen@f1r4s5.codam.nl>
2024-02-19 18:05:56 +0100

    struct

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: jiajchen <jiajchen@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/02/17 17:03:53 by jiajchen      #+#    #+#                 */
-/*   Updated: 2024/02/17 17:04:49 by jiajchen      ########   odam.nl         */
+/*   Updated: 2024/02/19 16:28:10 by jiajchen      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,44 @@
 # include <math.h>
 # include <MLX42/MLX42.h>
 # include <libft.h>
+
+typedef struct s_texture
+{
+	mlx_image_t	*no;
+	mlx_image_t	*so;
+	mlx_image_t	*we;
+	mlx_image_t	*ea;
+	int			ceil;
+	int			floor;
+}	t_texture;
+
+typedef struct s_map
+{
+	char		**map;
+	int			width;
+	int			height;
+	int			start_x;
+	int			start_y;
+}	t_map;
+
+typedef struct s_player
+{
+	int			p_x;
+	int			p_y;
+}	t_player;
+
+typedef struct s_ray
+{
+	/* data */
+}	t_ray;
+
+
+typedef struct s_cub
+{
+	mlx_t		*mlx;
+	mlx_image_t	*img;
+	t_player	*ply;
+	t_texture	*tex;
+}	t_cub;
 
 #endif
