3e9b59863f343f58b653c7fe9e9bb9a430678f41
Jiajia Chen <jiajchen@f1r2s17.codam.nl>
2024-03-07 13:56:23 +0100

    minimap setup

--- src/main.c
+++ src/main.c
@@ -6,11 +6,25 @@
 /*   By: jiajchen <jiajchen@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/02/19 12:48:15 by jiajchen      #+#    #+#                 */
-/*   Updated: 2024/03/07 10:41:36 by jiajchen      ########   odam.nl         */
+/*   Updated: 2024/03/07 13:17:54 by jiajchen      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
+
+void	minimap(t_cub *cub)
+{
+	int	i = -1;
+	int	j;
+	while (++i < 100)
+	{
+		j  = -1;
+		while (++j < 100)
+		{
+			mlx_put_pixel(cub->img, 5 + j, 5 + i, 0x00000000);
+		}
+	}
+}
 
 void	game_loop(void *param)
 {
@@ -21,6 +35,7 @@ void	game_loop(void *param)
 	cub->img = mlx_new_image(cub->mlx, WIDTH, HEIGHT);
 	cast_ray(cub);
 	move(cub);
+	minimap(cub);
 	mlx_image_to_window(cub->mlx, cub->img, 0, 0);
 }
 
