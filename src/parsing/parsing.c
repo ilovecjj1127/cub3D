187d2ce2bb5ab0709ea859914d8624c94adecbd6
Julia Caro <jcaro@f0r6s9.codam.nl>
2024-03-11 19:47:52 +0100

    check file extension

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -1,22 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        ::::::::            */
-/*   parsing.c                                          :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: jcaro <jcaro@student.42.fr>                  +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2024/03/03 13:33:04 by jcaro         #+#    #+#                 */
-/*   Updated: 2024/03/04 18:02:00 by jiajchen      ########   odam.nl         */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcaro <jcaro@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/03 13:33:04 by jcaro             #+#    #+#             */
+/*   Updated: 2024/03/11 19:43:49 by jcaro            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
+
+int	check_file_extension(char *file)
+{
+	char	*extension;
+
+	if (ft_strlen(file) < 5)
+		return (0);
+	extension = file + (ft_strlen(file) - 4);
+	return (ft_strncmp(extension, ".cub", 5) == 0);
+}
 
 int	parse_file(char *file, t_map *map_data, t_textures *textures)
 {
 	int	fd;
 	int	lines_read;
 
+	if (!check_file_extension(file))
+	{
+		printf("Error\nFile extension should be '.cub'\n");
+		return (0);
+	}
 	fd = open(file, O_RDONLY, 0644);
 	if (fd == -1)
 	{
